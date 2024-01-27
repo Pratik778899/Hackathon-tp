96848c8aa1f5290c73368fde60cc29b13e3ecb8b
Pratik778899 <pratik78877@gmail.com>
2024-01-27 10:12:21 +0530

    Added Folder Structur

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>EduCate</h1>
+        </div>
+        <div>
+          <h3>Parent Awwareness</h3>
+          <h3>last session</h3>
+        </div>
+        <div>
+          <h1>login</h1>
+          <h1>signup</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
